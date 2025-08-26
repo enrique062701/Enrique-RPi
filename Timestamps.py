3c867a63283a6c45168f5cd852c91a860f32516b
enrique062701 <enriquecisneros67@gmail.com>
2025-08-26 15:06:35 -0700

    Edits

--- Timestamps.py
+++ Timestamps.py
@@ -59,6 +59,7 @@ def plot_function(data, **kwargs):
     suffix = kwargs.get('plot')
     if kwargs.get('plot'):
         plotting_data = {key:value for key, value in data.items() if key.startswith(prefix) and key.endswith(suffix)}
+        print(plotting_data, 'These are the values inside plotting_data')
         for key, array in plotting_data.items():
             size = len(array)
             plt.plot(np.array(array), label = key)
@@ -84,53 +85,53 @@ def plot_function(data, **kwargs):
 if __name__ == "__main__":
     gain10 = "Enrique_gain1_10-2025-03-19.h5"
     gain15 = "Enrique_gain1_15-2025-03-19.h5"
-    gain20 = "Enrique_gain1_20-2025-03-19.h5"
-    gain25 = "Enrique_gain1_25-2025-03-19.h5"
-    gain30 = "Enrique_gain1_30-2025-03-19.h5"
-    gain35 = "Enrique_gain1_35-2025-03-19.h5"
-    gain40 = "Enrique_gain1_40-2025-03-19.h5"
-    gain45 = "Enrique_gain1_45-2025-03-19.h5"
-    gain50 = "Enrique_gain1_50-2025-03-19.h5"
+    #gain20 = "Enrique_gain1_20-2025-03-19.h5"
+    #gain25 = "Enrique_gain1_25-2025-03-19.h5"
+    #gain30 = "Enrique_gain1_30-2025-03-19.h5"
+    #gain35 = "Enrique_gain1_35-2025-03-19.h5"
+    #gain40 = "Enrique_gain1_40-2025-03-19.h5"
+    #gain45 = "Enrique_gain1_45-2025-03-19.h5"
+    #gain50 = "Enrique_gain1_50-2025-03-19.h5"
     
     gain10_timestamps = all_timestamps(gain10)
     gain15_timestamps = all_timestamps(gain15)
-    gain20_timestamps = all_timestamps(gain20)
-    gain25_timestamps = all_timestamps(gain25)
-    gain30_timestamps = all_timestamps(gain30)
-    gain35_timestamps = all_timestamps(gain35)
-    gain40_timestamps = all_timestamps(gain40)
-    gain45_timestamps = all_timestamps(gain45)
-    gain50_timestamps = all_timestamps(gain50)
+    #gain20_timestamps = all_timestamps(gain20)
+    #gain25_timestamps = all_timestamps(gain25)
+    #gain30_timestamps = all_timestamps(gain30)
+    #gain35_timestamps = all_timestamps(gain35)
+    #gain40_timestamps = all_timestamps(gain40)
+    #gain45_timestamps = all_timestamps(gain45)
+    #gain50_timestamps = all_timestamps(gain50)
     
     gain10_difference = difference(gain10_timestamps)
     gain15_difference = difference(gain15_timestamps)
-    gain20_difference = difference(gain20_timestamps)
-    gain25_difference = difference(gain25_timestamps)
-    gain30_difference = difference(gain30_timestamps)
-    gain35_difference = difference(gain35_timestamps)
-    gain40_difference = difference(gain40_timestamps)
-    gain45_difference = difference(gain45_timestamps)
-    gain50_difference = difference(gain50_timestamps)
+    #gain20_difference = difference(gain20_timestamps)
+    #gain25_difference = difference(gain25_timestamps)
+    #gain30_difference = difference(gain30_timestamps)
+    #gain35_difference = difference(gain35_timestamps)
+    #gain40_difference = difference(gain40_timestamps)
+    #gain45_difference = difference(gain45_timestamps)
+    #gain50_difference = difference(gain50_timestamps)
 
 
     plot_test = plot_function(gain10_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp', 
                                                          'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
     plot_test = plot_function(gain15_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
                                                      'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
-    plot_test = plot_function(gain20_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp', 
-                                                        'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
-    plot_test = plot_function(gain25_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
-                                                     'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
-    plot_test = plot_function(gain30_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
-                                                     'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
-    plot_test = plot_function(gain35_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp', 
-                                                     'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
-    plot_test = plot_function(gain40_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
-                                                     'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
-    plot_test = plot_function(gain45_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
-                                                     'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
-    plot_test = plot_function(gain50_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
-                                                     'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
+    #plot_test = plot_function(gain20_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp', 
+    #                                                    'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
+    #plot_test = plot_function(gain25_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
+    #                                                 'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
+    #plot_test = plot_function(gain30_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
+    #                                                 'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
+    #plot_test = plot_function(gain35_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp', 
+    #                                                 'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
+    #plot_test = plot_function(gain40_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
+    #                                                 'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
+    #plot_test = plot_function(gain45_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
+    #                                                 'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
+    #plot_test = plot_function(gain50_difference, plot = ('13PICAM2:cam1:IntensifierGain_RBV.timestamp',
+    #                                                 'LeCroy:Ch1:Trace.timestamp', 'LeCroy:Ch3:Trace.timestamp','LeCroy:Time.timestamp', 'MSO24:Ch1:Trace.timestamp'))
 
 
 
