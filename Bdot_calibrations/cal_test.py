a789026be1af12b6fe2a94c315a776df963ea05d
enrique062701 <enriquecisneros67@gmail.com>
2025-09-02 10:41:29 -0700

    Comments on how to use

--- Bdot_calibrations/cal_test.py
+++ Bdot_calibrations/cal_test.py
@@ -14,10 +14,12 @@ run_data = "magnet-calibration-pat-ground-2025-05-27.h5"
 
 Bdot = Bdot_actions(calibration_data, run_data) # This will intialize the class and place the data as objects
 Bdot.oscilloscope_data() # This will allow you to access all the files in the HDF5 file, not just the oscilloscope
-effective_area = Bdot.B_dot_calibration(imaginary = False)
+
+#This is how you get the effective_area
+effective_area = Bdot.B_dot_calibration(imaginary = False) #Set imaginary True if the .TXT files are in real and imaginary format instead of phase and frequency
 print(f'The effective area is: {effective_area}')
 
-#If you want to get the reconstructed field
+#If you want to get the reconstructed field. The effective area and other factors are taken into account after calculating effective area
 MSO24_Ch4 = Bdot.MSO24_Ch4_Trace
 MSO24_time = Bdot.MSO24_Time
 
