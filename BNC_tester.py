8b6cd39899e9752ab89b59618eeaec9da8be6511
enrique062701 <enriquecisneros67@gmail.com>
2025-10-08 16:35:30 -0700

    File for testing

--- BNC_tester.py
+++ BNC_tester.py
@@ -4,8 +4,9 @@ latency, the response time, exceptions, and accuracy.
 Enrique C.
 """
 import epics
-import numpy
-import os 
+import subprocess
+import numpy as np
+import threading
 import socket
 import time
 
@@ -57,14 +58,155 @@ channel_delays_limits = {
 
 # The next step is to initialize the dictionaries for the width of the waveform.
 # Can do width from 10 ns to 999.99999999975
-width_small = np.arange(0, 0.0000001, 0.000000001)
+width_small = np.arange(0, 0.0000001, 0.00000001)
 width_regular = np.arange(0, MAX, 1)
 
-delay = 0.01 #Delay to see how fast we can input the PVs without crashing the program. 
+channel_width = {
+    "Ch1" : width_regular,
+    "Ch2" : width_regular,
+    "Ch3" : width_regular,
+    "Ch4" : width_regular,
+    "Ch5" : width_regular,
+    "Ch6" : width_regular,
+    "Ch7" : width_regular,
+    "Ch8" : width_regular,
+}
+ 
+channel_width_small = {
+    "Ch1" : width_small,
+    "Ch2" : width_small,
+    "Ch3" : width_small,
+    "Ch4" : width_small,
+    "Ch5" : width_small,
+    "Ch6" : width_small,
+    "Ch7" : width_small,
+    "Ch8" : width_small,
+}
 
-for keys, items in channel_delays.items():
-    epics.caput(f"{USER}{keys}:Delay", items)
+channel_width_limits = {
+    "Ch1" : Delay_limits,
+    "Ch2" : Delay_limits,
+    "Ch3" : Delay_limits,
+    "Ch4" : Delay_limits,
+    "Ch5" : Delay_limits,
+    "Ch6" : Delay_limits,
+    "Ch7" : Delay_limits,
+    "Ch8" : Delay_limits,
+}
 
+
+
+#delay = 0.01 #Delay to see how fast we can input the PVs without crashing the program. 
+
+
+def delay_loop(dictionary, USER, delay):
+    delay_per_channel = []
+    count = 0
+    start_time = time.time() #Keep track of the time
+
+    for channel, values in dictionary.items():
+        start_time2 = time.time()
+        for value in values:
+            try:
+                epics.caput(f"{USER}{channel}:Delay", value)
+                time.sleep(delay)
+                count += 1 #This is so you can know which delay it crashed on by indexing later.
+                print(f"Done with the value: {value}")
+
+            except socket.error as e:
+                print(f'Socket error occured: {e}')
+
+            except Exception as e:
+                print(f"Error occured {e}. It occured at {count} with delay {value[count]}")
+
+        target_value = values[-1] # Gets the final value in the list
+        timeout = 100 # Timout after 100s.
+        precision = 1e-9 # Nanosecond precision for the BNC 575
+
+        while True:
+            current_value = epics.caget(f"{USER}{channel}:Delay_RBV")
+            if current_value is None:
+                print(f"Epics is not responding... Check for socket errors or EPICS errors.")
+            
+            elif abs(current_value - value) <= precision: #Doing absoulute difference instead of "==" bc of float precision. BNC limit is 10ns so ns precision is required.
+                total_time = time.time() - start_time2
+                delay_per_channel.append(total_time) # Append the time it takes for the device to catch up
+                print(f"The BNC has now caught up in values. The BNC RBV is: {current_value} and the value is: {value}")
+                break        
+
+        print(f"Done with channel: {channel}. It took a totla of {time.time() - start_time2} to complete")
+
+    print(f"Total time for this function to finish is: {time.time() - start_time}")
+    average = np.average(delay_per_channel)
+
+    return average
+
+
+def width_loop(dictionary, USER, delay):
+    width_per_channel = []
+    count = 0
+    start_time = time.time() #Keep track of the time
+
+    for channel, values in dictionary.items():
+        start_time2 = time.time()
+        for value in values:
+            try:
+                epics.caput(f"{USER}{channel}:Width", value)
+                time.sleep(delay)
+                count += 1 #This is so you can know which delay it crashed on by indexing later.
+                print(f"Done with the value: {value}")
+
+            except socket.error as e:
+                print(f'Socket error occured: {e}')
+
+            except Exception as e:
+                print(f"Error occured {e}. It occured at {count} with delay {value[count]}")
+
+        target_value = values[-1] # Gets the final value in the list
+        timeout = 100 # Timout after 100s.
+        precision = 1e-9 # Nanosecond precision for the BNC 575
+
+        while True:
+            current_value = epics.caget(f"{USER}{channel}:Width_RBV")
+            if current_value is None:
+                print(f"Epics is not responding... Check for socket errors or EPICS errors.")
+            
+            elif abs(current_value - value) <= precision: #Doing absoulute difference instead of "==" bc of float precision. BNC limit is 10ns so ns precision is required.
+                total_time = time.time() - start_time2
+                width_per_channel.append(total_time) # Append the time it takes for the device to catch up
+                print(f"The BNC has now caught up in values. The BNC RBV is: {current_value} and the value is: {value}")
+                break        
+
+        print(f"Done with channel: {channel}. It took a totla of {time.time() - start_time2} to complete")
+
+    print(f"Total time for this function to finish is: {time.time() - start_time}")
+    average = np.average(width_per_channel)
+
+    return average    
+
+            
+try:
+    delays = np.arange(0.01, 0.1, 0.01)
+    print(delays)
+    averages = []
+    for delay in delays:
+        reg_delay = delay_loop(channel_delays, USER, delay)
+        small_delay = delay_loop(channel_delays_small, USER, delay)
+        limits_delay = delay_loop(channel_delays_limits, USER, delay)
+        reg_width = width_loop(channel_width, USER, delay)
+        small_width = width_loop(channel_width_small, USER, delay)
+        limits_width = width_loop(channel_width_limits, USER, delay)
+        np.savez(f"Average_delay_delay_{delay}.npz", Delays_normal = reg_delay, Small_delay = small_delay, Limits_delay = limits_delay, Width_normal = reg_width
+        Small_width = small_width, Limits_width = limits_width)
+        
+
+
+
+
+
+except Exception as e:
+    print(f"An error occured: {e}")
+    
 
 
 
