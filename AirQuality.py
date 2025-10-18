b92edf8c15f749d6bb55b8ddb52773d1ae997fdc
enrique062701 <enriquecisneros67@gmail.com>
2025-10-18 16:57:41 -0700

    edits

--- AirQuality.py
+++ AirQuality.py
@@ -5,22 +5,48 @@ Will use epics to communicate.
 import time
 import board
 import adafruit_ahtx0
+import busio
 import inspect
-
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
 # Communication over the board's default I2C bus
 
 # First step is to create a sensor object
-#print(inspect.getsource(board))
+print(inspect.getsource(PM25_I2C))
 
+reset_pin = None
 
+i2c = busio.I2C(board.SCL, board.SDA, frequency = 100000)
 
-sensor = adafruit_ahtx0.AHTx0(board.I2C())
+pm25 = PM25_I2C(i2c, reset_pin)
+
+print("Found PM2.5 sensor, reading data...")
 
 while True:
-    print(sensor.temperature)
-    time.sleep(2)
+    time.sleep(1)
+    try:
+        aqdata = pm25.read()
 
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
 
-print('Test')
+    except KeyboardInterrupt:
+        break
+    print()
+    print("Connection Units (standard)")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    )
+    print("Concentration Units (environmental)")
+    print("---------------------")
+    print(
+        "PM 1.0 %d\tPM2.5: %d\tPM10: $d"
+        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+    )
+    print("---------------------")
+    print("Particles > 0.3um / 0.1L air: ", aqdata["particles 03um"])
+
 
 
